df4df1c85662fc9bf3e5ea721081c2b0e07ffdde
taylorsuccessor <taylorsuccessor@gmail.com>
2016-01-13 18:01:21 +0300

    marketWatch.blade.php

--- modules/Tools/Resources/views/client/marketWatch.blade.php
+++ modules/Tools/Resources/views/client/marketWatch.blade.php
@@ -451,14 +451,10 @@
         var minutes = hourObject.getMinutes();
 
 
-
         $(document).ready(function () {
             /*$('.over_hour_box_div').animate({'left': (hour * 4.166) + '%'}, 100, function () {
              });*/
 
-            var monthNames = ["January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"
-            ];
 
             var month = hourObject.getMonth();
             var day = hourObject.getDate();
@@ -466,7 +462,8 @@
             var year = hourObject.getFullYear();
             //city_date">Fri, Nov 17 2017
 
-            $('.city_date').html(day + ', ' + monthNames[month] + ' ' + (month+1) + ' ' + year);
+
+            $('.city_date').html(getFormatDate(year,month,day));
 
 
         });
@@ -493,11 +490,50 @@
     $(".clock_hours_row .left_label_div").append(city_label_and_hour_html(city_array));
 
 
+function getFormatDate(y,m,d){
+    
+            var monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+  return  d + ', ' + monthNames[m] + ' '  + y;
+}
+function getOtherCityDateFromHour(myHour,myGmt,otherGmt){
+
+    
+    var hourObject = new Date();
+            var month = hourObject.getMonth();
+            var day = hourObject.getDate();
+
+            var year = hourObject.getFullYear(); 
+            
+        
+        var otherHour=(myHour+(otherGmt - myGmt ))%24;
+        
+        if(otherHour >myHour && myGmt >otherGmt){
+            return getPastDayDate(year,month,day);
+        }
+        if(otherHour >myHour && myGmt >otherGmt){
+            return getNextDayDate(year,month,day);
+        }
+        
+        return getFormatDate(year,month,day);
+}
+function getPastDayDate(year,month,day){
+    
+    
+    return getFormatDate(year,month,day);
+}
+function getNextDayDate(year,month,day){
+    
+    
+    return getFormatDate(year,month,day);
+}
     function setTime(city_array) {
         for (var i = 0; i < city_array.length; i++) {
 
             var hourObject = new Date();
             var new_hour = hourObject.getHours() + (city_array[i][3] - 3);
+            
             hour = new_hour;
             minutes = hourObject.getMinutes();
 
@@ -511,6 +547,8 @@
             new_hour = (new_hour < 10) ? '0' + new_hour : new_hour;
             minutes = (minutes < 10) ? '0' + minutes : minutes;
             $('#city_info_all_div_' + i + ' .city_hour').html(new_hour + ':' + minutes + '<span>' + am_bm + '</span>');
+            
+            $('#city_info_all_div_' + i + ' .city_date').html(getOtherCityDateFromHour(hourObject.getHours(),realOffset,city_array[i][3]));
         }
     }
     setTime(city_array);
