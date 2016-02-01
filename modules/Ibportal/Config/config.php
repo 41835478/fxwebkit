5749752740632cb249e5609edbd517c8e45f0f0f
taylorsuccessor <taylorsuccessor@gmail.com>
2016-02-01 15:06:00 +0300

    error

--- modules/Ibportal/Config/config.php
+++ modules/Ibportal/Config/config.php
@@ -3,6 +3,15 @@
 return [
 	'is_admin' => 1,
 	'is_client' => 1,
+	'route' => 'admin.ibportal.planList',
+	'icon' => 'fa-briefcase',
+],
+	[
+		'route' => 'admin.ibportal.aliasesList',
+		'title' => 'aliases',
+		'icon' => 'fa-random',
+	]
+
 	'name' => 'Ibportal',
 	'icon' => 'fa-user',
 	'route' => '',
@@ -10,19 +19,10 @@ return [
 		'50'=> 'type',
 		'100'=>'type',
 		'150'=>'type',
-	],
+],
 	'admin_menu' => [
-		[
-			'route' => 'admin.ibportal.planList',
-			'title' => 'plans',
-			'icon' => 'fa-briefcase',
-		],
-		[
-		'route' => 'admin.ibportal.aliasesList',
-		'title' => 'aliases',
-		'icon' => 'fa-random',
-	]
 
+			'title' => 'plans',[
 	],
 	'client_menu' => [
 		[
