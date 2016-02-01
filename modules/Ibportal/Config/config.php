fbf4a92be80e052a8e07b33b2740ebc0753124dc
mag <mag@mqplanet.com>
2016-02-01 15:13:04 +0200

    as

--- modules/Ibportal/Config/config.php
+++ modules/Ibportal/Config/config.php
@@ -1,34 +1,34 @@
 <?php
 
 return [
-	'is_admin' => 1,
-	'is_client' => 1,
-	'name' => 'Ibportal',
-	'icon' => 'fa-user',
-	'route' => '',
-	'type'=>[
-		'50'=> 'type',
-		'100'=>'type',
-		'150'=>'type',
-	],
-	'admin_menu' => [
-		[
-			'route' => 'admin.ibportal.plansList',
-			'title' => 'plans',
-			'icon' => 'fa-briefcase',
-		],
-		[
-		'route' => 'admin.ibportal.aliasesList',
-		'title' => 'aliases',
-		'icon' => 'fa-random',
-	]
+    'is_admin' => 1,
+    'is_client' => 1,
+    'name' => 'Ibportal',
+    'icon' => 'fa-user',
+    'route' => '',
+    'type' => [
+        '50' => 'type',
+        '100' => 'type',
+        '150' => 'type',
+    ],
+    'admin_menu' => [
+        [
+            'route' => 'admin.ibportal.plansList',
+            'title' => 'plans',
+            'icon' => 'fa-briefcase',
+        ],
+        [
+            'route' => 'admin.ibportal.aliasesList',
+            'title' => 'aliases',
+            'icon' => 'fa-random',
+        ]
 
-	],
-	'client_menu' => [
-		[
-		'route' => 'client.ibportal.planList',
-		'title' => 'plans',
-		'icon' => 'fa-briefcase',
-	]
-	]
+    ],
+    'client_menu' => [
+        [
+            'route' => 'client.ibportal.planList',
+            'title' => 'plans',
+            'icon' => 'fa-briefcase',
+        ]
+    ]
 ];
