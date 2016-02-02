7a0e3b47ae7d215a2794fbce985166d56c157972
taylorsuccessor <taylorsuccessor@gmail.com>
2016-02-02 11:01:25 +0300

    start to get agents users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,11 @@
 
 use Cartalyst\Sentinel\Users\EloquentUser as SentinelUser;
 
+
 class User extends SentinelUser
 {
 	//
-
+public function agentUsers(){
+    return $this->hasMany('Modules\Ibportal\Entities\IbportalAgentUser','agent_id')
+}
 }
