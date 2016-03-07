ccdccfa08efd2b80f68fb1362015a27723f08b2e
taylorsuccessor <taylorsuccessor@gmail.com>
2016-03-07 15:30:09 +0200

    edit migration

--- modules/Request/Database/Migrations/2016_03_06_125819_create_request_assign_account_table.php
+++ modules/Request/Database/Migrations/2016_03_06_125819_create_request_assign_account_table.php
@@ -16,7 +16,7 @@ class CreateRequestAssignAccountTable extends Migration {
         {
             $table->increments('id');
 
-            $table->integer('accountId',100);
+            $table->integer('accountId');
             $table->string('name',100);
             $table->string('login',100);
             $table->string('password',100);
