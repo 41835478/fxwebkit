c71fe4f31457d1078156e3806da4f5e2ca0be29a
taylor <taylorsuccessor@gmail.com>
2016-03-12 03:56:06 +0200

    add Mt4TradeSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeds\CmsSeeder;
 include base_path().'\Database\Seeds\CmsSeeder.php';
+include base_path().'\Database\Seeds\Mt4TradeSeeder.php';
+
 
 
 class DatabaseSeeder extends Seeder {
@@ -19,6 +21,7 @@ class DatabaseSeeder extends Seeder {
 
 		$this->call('UsersTableSeeder');
 		$this->call('CmsSeeder');
+		$this->call('Mt4TradeSeeder');
 	}
 
 }
