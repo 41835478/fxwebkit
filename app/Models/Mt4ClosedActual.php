cc618d9419493c456b199d7220a3fc7d3ebdaf71
Galya <galya@mqplanet.com>
2016-03-14 12:20:43 +0200

    Default mail to read from .ENV and Adding USers() to some model classes

--- app/Models/Mt4ClosedActual.php
+++ app/Models/Mt4ClosedActual.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class Mt4ClosedActual extends Model
 {
     protected $table='mt4_closed_actual';
+
+    public function users(){
+        $this->primaryKey='login';
+        return $this->belongsToMany('Fxweb\Models\User', 'mt4_users_users', 'mt4_users_id','users_id','login' );
+    }
 }
--- app/Models/Mt4OpenActual.php
+++ app/Models/Mt4OpenActual.php
@@ -5,4 +5,10 @@ use Illuminate\Database\Eloquent\Model;
 class Mt4OpenActual extends Model
 {
    protected $table='mt4_open_actual';
+
+   public function users(){
+      $this->primaryKey='login';
+      return $this->belongsToMany('Fxweb\Models\User', 'mt4_users_users', 'mt4_users_id','users_id','login' );
+   }
+
 }
