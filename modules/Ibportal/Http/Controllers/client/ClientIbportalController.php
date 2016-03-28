5b23eb57b042668b4b6dabaff01e6d06fa9dc7a4
taylorsuccessor <taylorsuccessor@gmail.com>
2016-03-28 15:59:52 +0200

    if agent does not has mt4 user to get his commission

--- modules/Ibportal/Http/Controllers/client/ClientIbportalController.php
+++ modules/Ibportal/Http/Controllers/client/ClientIbportalController.php
@@ -266,11 +266,9 @@ class ClientIbportalController extends Controller
     {
 
         $login=UserIbid::select('login')->where('user_id', current_user()->getUser()->id)->first();
-        if($login){
-            $login=$login->login;
-        }else{
-            /* TODO please determine if the user does not have mt4 login to get his commission(IBID table column field)*/
-            dd('please determine if the user does not have mt4 login to get his commission(IBID table column field)');}
+
+            $login=($login)?$login->login:-1;
+
         $oSymbols = $this->Ibportal->getClosedTradesSymbols();
         $aTradeTypes = ['' => 'ALL'] + $this->Ibportal->getAgentCommissionTypes();
         $serverTypes = $this->Ibportal->getServerTypes();
