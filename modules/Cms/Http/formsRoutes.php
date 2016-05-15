b7fc3d4db95caabdb92205b53118484dc25070bb
taylorsuccessor <taylorsuccessor@gmail.com>
2016-05-15 08:59:06 +0300

    forms test

--- modules/Cms/Http/formsRoutes.php
+++ modules/Cms/Http/formsRoutes.php
@@ -8,4 +8,11 @@ Route::get('posts/form', [
 ]);
 Route::post('posts/form', [
     'uses' => 'Modules\Cms\Http\Controllers\forms\postsController@cms_store'
+]);Route::group(['middleware' => ['authenticate.admin'],'prefix' => 'cms', 'namespace' => ''], function() {
+Route::resource('pofffsts', 'Modules\Cms\Http\Controllers\forms\pofffstsController');});
+Route::get('pofffsts/form', [
+    'as' => 'pofffsts.form', 'uses' => 'Modules\Cms\Http\Controllers\forms\pofffstsController@cms_create'
+]);
+Route::post('pofffsts/form', [
+    'uses' => 'Modules\Cms\Http\Controllers\forms\pofffstsController@cms_store'
 ]);
