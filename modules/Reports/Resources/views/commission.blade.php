e14c075f49acbf222e94bcdbaf19d174e1086432
taylorsuccessor <taylorsuccessor@gmail.com>
2016-11-13 16:16:25 +0200

    commission high chart for admin area

--- modules/Reports/Resources/views/commission.blade.php
+++ modules/Reports/Resources/views/commission.blade.php
@@ -84,7 +84,8 @@
                 </div>
             <div class="padding-xs-vr"></div>
             @endif
-
+            <div id="container"></div>
+            <div id="container2"></div>
             <div class="table-light">
                 <div class="table-header">
                     <div class="table-caption">
@@ -139,6 +140,11 @@
     </div>
 </div>
 </div>
+    @stop
+@section('script')
+    @parent
+
+    {!! HTML::script('assets/'.config('fxweb.layoutAssetsFolder').'/js/highcharts.js') !!}
 <script>
     init.push(function () {
         var options = {
@@ -194,6 +200,100 @@
             $("#from_login_li,#to_login_li").show();
             $("#login_li").hide();
         }
+    });
+
+
+
+
+    $(function () {
+        $('#container').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: {!! json_encode($chartData)!!}
+            }]
+        });
+    });
+
+
+
+
+
+    $(function () {
+        $('#container2').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'World\'s largest cities per 2014'
+            },
+            subtitle: {
+                text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (millions)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>'
+            },
+            series: [{
+                name: 'Population',
+                data:  {!! json_encode($chartData2)!!},
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 10, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            }]
+        });
     });
 </script>
 @stop
