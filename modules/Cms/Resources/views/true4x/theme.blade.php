fa11c6e43f601a54d2c3f0da90a8a13de78a80f9
taylorsuccessor <taylorsuccessor@gmail.com>
2016-12-11 04:55:05 -0800

    true4x template cms

--- modules/Cms/Resources/views/true4x/theme.blade.php
+++ modules/Cms/Resources/views/true4x/theme.blade.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <title>True4x</title>
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset($asset_folder.'true4x') }}/public/css/main.css"  >
+    <title>True4x</title>
 
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" >
 
     <meta charset="UTF-8">
 
 
 
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset($asset_folder.'true4x') }}/public/css/main.css"  >
 
 
 </head>
@@ -22,53 +19,66 @@
 
 
 
-@if(isset($positions["house_main_slider"]) )
-    @foreach($positions['house_main_slider'] as $position)
-        {!! $position !!}
-    @endforeach
-@endif
-
-
-
-    @if(isset($positions["house_top_banner"]) )
-        @foreach($positions['house_top_banner'] as $position)
+<header>
+    @if(isset($positions["true4x_top_header_line"]) )
+        @foreach($positions['true4x_top_header_line'] as $position)
             {!! $position !!}
         @endforeach
     @endif
 
-    <div class="container" style="padding:0px; width:100%;">
-@if(isset($positions["house_aside"]) || isset($positions["house_body"]) )
-
-        <div class="container">
-
-<div id="sidebar"class="col-xs-3">
-            @if(isset($positions["house_aside"]) )
-                @foreach($positions['house_aside'] as $position)
-                    {!! $position !!}
-                @endforeach
-            @endif
-</div>
 
 
 
-            <div id="page-contents" class=" @if(isset($positions["house_aside"]) )col-xs-9 @else col-xs-12  @endif" >
+    @if(isset($positions["true4x_header"]) )
+        @foreach($positions['true4x_header'] as $position)
+            {!! $position !!}
+        @endforeach
+    @endif
 
-
-                @if(isset($positions["house_body"]) )
-                    @foreach($positions['house_body'] as $position)
-                        {!! $position !!}
-                    @endforeach
-                @endif
-            </div><!--#page-contents-->
-
-        </div><!--.container-->
-@endif
+    @if(isset($positions["true4x_main_slider"]) )
+        @foreach($positions['true4x_main_slider'] as $position)
+            {!! $position !!}
+        @endforeach
+    @endif
 
 
 
 
 
+</header>
 
+
+
+<section>
+
+
+    <div class="row">
+        @if(isset($positions["true4x_aside"]) || isset($positions["true4x_body"]) )
+
+            <div class="container">
+
+                <div id="sidebar"class="col-xs-3">
+                    @if(isset($positions["true4x_aside"]) )
+                        @foreach($positions['true4x_aside'] as $position)
+                            {!! $position !!}
+                        @endforeach
+                    @endif
+                </div>
+
+
+
+                <div id="page-contents" class=" @if(isset($positions["true4x_aside"]) )col-xs-9 @else col-xs-12  @endif" >
+
+
+                    @if(isset($positions["true4x_body"]) )
+                        @foreach($positions['true4x_body'] as $position)
+                            {!! $position !!}
+                        @endforeach
+                    @endif
+                </div><!--#page-contents-->
+
+            </div><!--.container-->
+        @endif
     </div>
 
 
@@ -76,26 +86,26 @@
 
 
 
-@if(isset($positions["house_position_1"]) )
-    @foreach($positions['house_position_1'] as $position)
-        {!! $position !!}
-    @endforeach
-@endif
+    @if(isset($positions["true4x_position_1"]) )
+        @foreach($positions['true4x_position_1'] as $position)
+            {!! $position !!}
+        @endforeach
+    @endif
 
-@if(isset($positions["house_footer_upper"]) )
-    @foreach($positions['house_footer_upper'] as $position)
-        {!! $position !!}
-    @endforeach
-@endif
-
-
-@if(isset($positions["house_footer"]) )
-    @foreach($positions['house_footer'] as $position)
-        {!! $position !!}
-    @endforeach
-@endif
+    @if(isset($positions["true4x_footer_upper"]) )
+        @foreach($positions['true4x_footer_upper'] as $position)
+            {!! $position !!}
+        @endforeach
+    @endif
 
 
+    @if(isset($positions["true4x_footer"]) )
+        @foreach($positions['true4x_footer'] as $position)
+            {!! $position !!}
+        @endforeach
+    @endif
+
+</section>
 </body>
 
 
