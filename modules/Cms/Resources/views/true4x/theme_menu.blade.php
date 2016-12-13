c4ab74572c19cd62b08340535afebfe67f755b50
taylorsuccessor <taylorsuccessor@gmail.com>
2016-12-13 03:27:59 -0800

    style inner pages

--- modules/Cms/Resources/views/true4x/theme_menu.blade.php
+++ modules/Cms/Resources/views/true4x/theme_menu.blade.php
@@ -1,38 +1,70 @@
+<div class="row" id="headerMenuRow">
+    <div class="container">
+        <div id="mainMenuDiv">
+            <ul id="mainMenuUl">
+                @foreach($menu_array as $menu)
+
+                    <li class=" @if($menu['id'] == $selected_id )  active @endif "><a href="{{ asset($menu['name']) }}">{{ str_replace('-',' ',$menu['translate']) }}</a>
+                        @if(isset($menu['children']) && $menu['children']!= null)
+                        <div class="zeroSubmenuDiv">
+                            <div class="subMenuDiv">
+
+                                <ul class="submenuUl">
+                                    @foreach($menu['children'] as $sub_menu)
+                                    <li class="@if($sub_menu['id'] == $selected_id )   active @endif" >
+                                        <a href="{{ asset(strtolower($sub_menu['name'])) }}">
+                                            {{  str_replace('-',' ',$sub_menu['translate']) }}
+                                        </a> </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                        @endif
+
+                    </li>
+                @endforeach
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="horizLine">&nbsp;</div>
+</div>
 
 
-<ul class="b-top-nav__1level_wrap ">
+{{--<ul class="b-top-nav__1level_wrap ">--}}
 
-    @foreach($menu_array as $menu)
+    {{--@foreach($menu_array as $menu)--}}
 
-    <li class=" b-top-nav__1level f-top-nav__1level  f-primary-b  @if($menu['id'] == $selected_id )  active @endif ">
-        <a href='{{ asset($menu['name']) }}' onclick="return false;" >
+    {{--<li class=" b-top-nav__1level f-top-nav__1level  f-primary-b  @if($menu['id'] == $selected_id )  active @endif ">--}}
+        {{--<a href='{{ asset($menu['name']) }}' onclick="return false;" >--}}
 
-            <i class="fa fa-folder-open b-menu-1level-ico"></i>{{ str_replace('-',' ',$menu['translate']) }}<span class="b-ico-dropdown"><i class="fa fa-arrow-circle-down"></i></span>
+            {{--<i class="fa fa-folder-open b-menu-1level-ico"></i>{{ str_replace('-',' ',$menu['translate']) }}<span class="b-ico-dropdown"><i class="fa fa-arrow-circle-down"></i></span>--}}
 
-        </a>
-
-
-        @if(isset($menu['children']) && $menu['children']!= null)
-            <div class="b-top-nav__dropdomn" style="display:none;opacity:0; ">
-                <ul class=" b-top-nav__2level_wrap ">
-
-                @foreach($menu['children'] as $sub_menu)
-                        <li class=" b-top-nav__2level f-top-nav__2level f-primary  @if($sub_menu['id'] == $selected_id )   active @endif">
-                            <a href='{{ asset(strtolower($sub_menu['name'])) }}' >
-                                {{  str_replace('-',' ',$sub_menu['translate']) }}
-                            </a>
-                        </li>
-                          @endforeach
-                <ul>
-            </div>
-
-        @endif
+        {{--</a>--}}
 
 
+        {{--@if(isset($menu['children']) && $menu['children']!= null)--}}
+            {{--<div class="b-top-nav__dropdomn" style="display:none;opacity:0; ">--}}
+                {{--<ul class=" b-top-nav__2level_wrap ">--}}
 
-    </li>
+                {{--@foreach($menu['children'] as $sub_menu)--}}
+                        {{--<li class=" b-top-nav__2level f-top-nav__2level f-primary  @if($sub_menu['id'] == $selected_id )   active @endif">--}}
+                            {{--<a href='{{ asset(strtolower($sub_menu['name'])) }}' >--}}
+                                {{--{{  str_replace('-',' ',$sub_menu['translate']) }}--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                          {{--@endforeach--}}
+                {{--<ul>--}}
+            {{--</div>--}}
+
+        {{--@endif--}}
 
 
-    @endforeach
 
-</ul>
+    {{--</li>--}}
+
+
+    {{--@endforeach--}}
+
+{{--</ul>--}}
