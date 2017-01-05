401f522bb1661744120ae2ac9dca0b12b9a25a1a
taylorsuccessor <taylorsuccessor@gmail.com>
2017-01-05 01:26:24 -0800

    inner pages for true4x

--- modules/Cms/Resources/views/true4x/theme.blade.php
+++ modules/Cms/Resources/views/true4x/theme.blade.php
@@ -1,116 +1,396 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>True4x</title>
 
 
-    <meta charset="UTF-8">
+<div id="aboutUsPage" class="page" style="display: none;" >
+    <div class="innerContainer">
 
 
-
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset($asset_folder.'true4x') }}/public/css/main.css"  >
-
-
-</head>
-
-<body>
-
-
-
-<header>
-    @if(isset($positions["true4x_top_header_line"]) )
-        @foreach($positions['true4x_top_header_line'] as $position)
-            {!! $position !!}
-        @endforeach
-    @endif
-
-
-
-
-    @if(isset($positions["true4x_header"]) )
-        @foreach($positions['true4x_header'] as $position)
-            {!! $position !!}
-        @endforeach
-    @endif
-
-    @if(isset($positions["true4x_main_slider"]) )
-        @foreach($positions['true4x_main_slider'] as $position)
-            {!! $position !!}
-        @endforeach
-    @endif
+        </div>
+    </
 
 
 
 
 
-</header>
+<div id="aboutUsPage" class="page" style="display: none;" >
+    <div class="innerContainer">
+        <style type="text/css">
+
+            #aboutUsPage .mainInnerHeaderText img{
+                float:right;
+                margin:10px 10px 10px 10px;
+            }
+        </style>
+        <div class="mainInnerHeaderText">
+            <div class="innerPageHeader">
+                <h2>About Us</h2>
+            </div>
+            <p >
+                <img src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_06.png">
+                At True4x, we are dedicated to serving our clients responsibly with the latest innovations in forex tools and resources to assist you in trading. Below we underline the active promotions, bonuses and
+                <br>  <br>
+                Special offers available to traders. To take advantage of one of the offers listed below, simply register for an account, if you do not have one yet open with True4x, and contact your account manager for more information.
+
+
+            </p>
+
+        </div>
+
+        <div class="innerHLine"></div>
+
+
+        <style type="text/css">
+
+            #aboutUsPage #enhanceSection{
+
+            }
+
+
+            #aboutUsPage #enhanceSection .leftSideColumn{
+float:left;
+                width:30%;
+
+            }
+            #aboutUsPage #enhanceSection .leftSideColumn img{
+                margin-top:75px;
+                max-width: 95%;
+            }
+
+            #aboutUsPage #enhanceSection .rightSection{
+                float:left;
+                width:70%;
+            }
+            #aboutUsPage #enhanceSection    .menuWithDots li{
+width:100%;
+                display:block;
+                height: 50px;
+                position: relative;
+            }
+            #aboutUsPage #enhanceSection    .menuWithDots li img{
+position:absolute;
+                top:-20px;
+
+            }
+
+        </style>
+<div class="innerSectionWithHeader" id="enhanceSection">
+    <header>
+        <h3>Here’s how Forex Trading with True4x will enhance your trading experience:</h3>
+    </header>
+    <section>
+        <div class="leftSideColumn">
+            <img src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_17.png">
+        </div>
+
+
+            <div class="rightSection">
+                <ul class="menuWithDots">
+                    <li>
+                        Best-in-class customer service, including live chat
+                        <img  style="right:10px;"src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_10.png">
+                        </li>
+                    <li  >
+                        One-hour response time for sales and support requests
+                        <img style="right:60px;"src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_14.png">
+                    </li>
+                    <li >
+                        Multiple asset trading accounts on MT4
+                        <img  style="right:120px;"src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_24.png">
+                    </li>
+                    <li >
+                        Fast quality of execution
+                        <img  style="right:120px;"src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_20.png">
+                    </li>
+                    <li >
+                        Deep liquidity
+                        <img  style="right:60px;"src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_28.png">
+                    </li>
+                    <li >
+                        Islamic (Swap Free) accounts
+                        <img  style="right:10px;"src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_31.png">
+                    </li>
+                </ul>
+            </div>
+    </section>
 
 
 
-<section>
+</div>
+
+        <div class="innerHLine"></div>
+<style type="text/css">
+
+    #aboutUsPage #ourMission section{
+
+    }
+
+    #aboutUsPage #ourMission section img{
+float:left;
+        margin-right:20px;
+        min-width: 45%;
+    }
+
+    #aboutUsPage #ourMission section p{
+
+    }
+</style>
+
+        <div class="innerSectionWithHeader" id="ourMission">
+            <header>
+                <h3>Our Mission</h3>
+            </header>
+            <section>
+                    <img src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_37.png">
 
 
-    <div class="row">
-        @if(isset($positions["true4x_aside"]) || isset($positions["true4x_body"]) )
+                      Through superior customer service we aim to establish ourselves as a global brand by ensuring that the profitability, success and happiness of our customers are core to our business.
 
-            <div class="container">
-
-
-
-                <div id="page-contents" class=" @if(isset($positions["true4x_aside"]) )col-xs-9 @else col-xs-12  @endif" >
-
-
-                    @if(isset($positions["true4x_body"]) )
-                        @foreach($positions['true4x_body'] as $position)
-                            {!! $position !!}
-                        @endforeach
-                    @endif
-                </div><!--#page-contents-->
+            </section>
 
 
 
-                <div id="sidebar"class="col-xs-3">
-                    @if(isset($positions["true4x_aside"]) )
-                        @foreach($positions['true4x_aside'] as $position)
-                            {!! $position !!}
-                        @endforeach
-                    @endif
-                </div>
+        </div>
 
 
-            </div><!--.container-->
-        @endif
+        <div class="innerHLine"></div>
+
+        <style type="text/css">
+
+            #aboutUsPage #ourGoal section{
+
+            }
+
+            #aboutUsPage #ourGoal section img{
+                float:right;
+                margin-left:20px;
+            }
+
+            #aboutUsPage #ourGoal section p{
+
+            }
+        </style>
+        <div class="innerSectionWithHeader" id="ourGoal">
+            <header>
+                <h3>Our Goal</h3>
+            </header>
+            <section>
+                <img src="/cms_assets/true4x/public/images/inner_pages/about_us/True4x-About-Us_41.png">
+
+                As we continue to grow internationally and establish ourselves as a global brand, our main priority will remain your profitability, your success and your happiness as our clients – and it always will be. Being an industry leader is more than just leveraging the latest technologies. Our experienced staff is made up of a group of honest, reliable and hardworking professionals whose goal is all-encompassing support of our clients. With our priority of immediate response to your questions and willingness to expand both our knowledge base and yours, we are propelling True4x to a market leader in innovation and customer service. Fully licensed and regulated, our trading expertise encompasses currencies, precious metals, commodities, CFDs.
+
+            </section>
+
+
+
+        </div>
+
+
+
+
+
     </div>
 
 
+</div>
+
+
+<style type="text/css">
+.page{
+width:700px;
+}
+
+    .innerContainer{
+
+    }
+.page .mainInnerHeaderText .innerPageHeader{
+
+    color:#089594;
+
+}
+.page .mainInnerHeaderText .innerPageHeader:before{
+    content:" ";
+    display:block;
+    width:100%;
+    margin-top:10px;
+    border-bottom:5px solid #089594;
+
+ }
+.page .mainInnerHeaderText .innerPageHeader:after{
+    clear:both;
+    width:100%;
+    display:block;
+    content:" ";
+}
+.page .mainInnerHeaderText .innerPageHeader h2{
+float:left;
+    margin-top:-20px;
+    display:inline-block;
+    padding:5px;background-color: #ffffff;
+
+}
+
+.page .mainInnerHeaderText p{
+    clear:both;
+}
+    .innerSectionWithHeader{
+
+    }
+.innerSectionWithHeader h3{
+    color:#089594;
+    font-size: 18px;
+}
+    .innerHLine{
+        width:100%;
+        line-height: 0px;
+    border-bottom:2px solid #44596a;
+        margin:5px 0px;
+        clear: both;
+    }
+
+
+#aboutUsPage {
+
+}
+.menuWithDots{
+margin:0px;
+    padding: 0px;
+}
+.menuWithDots li{
+    list-style: none;
+
+}
+.menuWithDots li:before{
+content:"• ";
+    margin-right:5px;
+    margin-top:-5px;
+    font-size: 24px;
+    float:left;
+    color:#089594;
+}
+</style>
 
 
 
 
-    @if(isset($positions["true4x_position_1"]) )
-        @foreach($positions['true4x_position_1'] as $position)
-            {!! $position !!}
-        @endforeach
-    @endif
-
-    @if(isset($positions["true4x_footer_upper"]) )
-        @foreach($positions['true4x_footer_upper'] as $position)
-            {!! $position !!}
-        @endforeach
-    @endif
-
-
-    @if(isset($positions["true4x_footer"]) )
-        @foreach($positions['true4x_footer'] as $position)
-            {!! $position !!}
-        @endforeach
-    @endif
-
-</section>
-</body>
 
 
 
-</html>
+
+
+
+
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--<head>--}}
+    {{--<title>True4x</title>--}}
+
+
+    {{--<meta charset="UTF-8">--}}
+
+
+
+    {{--<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">--}}
+    {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">--}}
+    {{--<link rel="stylesheet" href="{{ asset($asset_folder.'true4x') }}/public/css/main.css"  >--}}
+
+
+{{--</head>--}}
+
+{{--<body>--}}
+
+
+
+{{--<header>--}}
+    {{--@if(isset($positions["true4x_top_header_line"]) )--}}
+        {{--@foreach($positions['true4x_top_header_line'] as $position)--}}
+            {{--{!! $position !!}--}}
+        {{--@endforeach--}}
+    {{--@endif--}}
+
+
+
+
+    {{--@if(isset($positions["true4x_header"]) )--}}
+        {{--@foreach($positions['true4x_header'] as $position)--}}
+            {{--{!! $position !!}--}}
+        {{--@endforeach--}}
+    {{--@endif--}}
+
+    {{--@if(isset($positions["true4x_main_slider"]) )--}}
+        {{--@foreach($positions['true4x_main_slider'] as $position)--}}
+            {{--{!! $position !!}--}}
+        {{--@endforeach--}}
+    {{--@endif--}}
+
+
+
+
+
+{{--</header>--}}
+
+
+
+{{--<section>--}}
+
+
+    {{--<div class="row">--}}
+        {{--@if(isset($positions["true4x_aside"]) || isset($positions["true4x_body"]) )--}}
+
+            {{--<div class="container">--}}
+
+
+
+                {{--<div id="page-contents" class=" @if(isset($positions["true4x_aside"]) )col-xs-9 @else col-xs-12  @endif" >--}}
+
+
+                    {{--@if(isset($positions["true4x_body"]) )--}}
+                        {{--@foreach($positions['true4x_body'] as $position)--}}
+                            {{--{!! $position !!}--}}
+                        {{--@endforeach--}}
+                    {{--@endif--}}
+                {{--</div><!--#page-contents-->--}}
+
+
+
+                {{--<div id="sidebar"class="col-xs-3">--}}
+                    {{--@if(isset($positions["true4x_aside"]) )--}}
+                        {{--@foreach($positions['true4x_aside'] as $position)--}}
+                            {{--{!! $position !!}--}}
+                        {{--@endforeach--}}
+                    {{--@endif--}}
+                {{--</div>--}}
+
+
+            {{--</div><!--.container-->--}}
+        {{--@endif--}}
+    {{--</div>--}}
+
+
+
+
+
+
+    {{--@if(isset($positions["true4x_position_1"]) )--}}
+        {{--@foreach($positions['true4x_position_1'] as $position)--}}
+            {{--{!! $position !!}--}}
+        {{--@endforeach--}}
+    {{--@endif--}}
+
+    {{--@if(isset($positions["true4x_footer_upper"]) )--}}
+        {{--@foreach($positions['true4x_footer_upper'] as $position)--}}
+            {{--{!! $position !!}--}}
+        {{--@endforeach--}}
+    {{--@endif--}}
+
+
+    {{--@if(isset($positions["true4x_footer"]) )--}}
+        {{--@foreach($positions['true4x_footer'] as $position)--}}
+            {{--{!! $position !!}--}}
+        {{--@endforeach--}}
+    {{--@endif--}}
+
+{{--</section>--}}
+{{--</body>--}}
+
+
+
+{{--</html>--}}
