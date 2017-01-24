76a1104ae534c6a1329666447ef70eb730a3e351
taylorsuccessor <taylorsuccessor@gmail.com>
2017-01-24 06:16:12 -0800

    test

--- commit.bat
+++ commit.bat
@@ -1,8 +1,7 @@
-E:
-cd /wamp/www/fxwebkit
+
 git add .
 git commit -am "%1"
 
- git pull origin master --repo https://m.hashim:complex@123@office.mqplanet.com:/var/projects/applications/fxwebkit
- git push origin master  --repo https://m.hashim:complex@123@office.mqplanet.com:/var/projects/applications/fxwebkit
+ git pull origin master
+ git push origin master
 
