3fb76448790b7090873eb53bfdd015e31984c0ad
taylorsuccessor <taylorsuccessor@gmail.com>
2017-01-24 05:37:03 -0800

    commit.bat

--- commit.bat
+++ commit.bat
@@ -1,13 +1,12 @@
 
 E:
 
-cd /wamp/www/fxwebkit
+cd /wamp/www/fxwebkit3
 
 git add .
 git commit -am "%0"
 
 
-echo complex@123 | git pull origin master;
-echo complex@123 | git push origin master;
-echo Press Enter...;
+echo complex@123 | git pull origin master
+echo complex@123 | git push origin master
 
